15976cdef65f021fcf2bc7848d956d4d52349bdd
0xsubnn17 <ahvha3vu@gmail.com>
2024-06-20 23:29:16 -0400

    refactor: improve API type definition with typeof

--- packages/penx/src/apiTypes.ts
+++ packages/penx/src/apiTypes.ts
@@ -2,78 +2,69 @@ import * as dialog from '@tauri-apps/plugin-dialog'
 import * as fs from '@tauri-apps/plugin-fs'
 import notification from '@tauri-apps/plugin-notification'
 import * as os from '@tauri-apps/plugin-os'
+import * as clipboard from 'tauri-plugin-clipboard-api'
 import * as shellx from 'tauri-plugin-shellx-api'
 import { FetchSendResponse, Proxy } from './fetch/types'
 
 export interface IDialog {
-  ask: (...args: Parameters<typeof dialog.ask>) => ReturnType<typeof dialog.ask>
-  confirm: (...args: Parameters<typeof dialog.confirm>) => ReturnType<typeof dialog.confirm>
-  message: (...args: Parameters<typeof dialog.message>) => ReturnType<typeof dialog.message>
+  ask: typeof dialog.ask
+  confirm: typeof dialog.confirm
+  message: typeof dialog.message
   open: (options?: dialog.OpenDialogOptions) => ReturnType<typeof dialog.open>
-  save: (options?: dialog.SaveDialogOptions) => ReturnType<typeof dialog.save>
+  save: typeof dialog.save
 }
 
 export interface IClipboard {
-  readText: () => Promise<string>
-  writeText: (text: string) => Promise<void>
-  readImageBase64: () => Promise<string>
-  writeImageBase64: (base64: string) => Promise<void>
-  readFiles: () => Promise<string[]>
-  writeFiles: (files: string[]) => Promise<void>
-  readRtf: () => Promise<string>
-  writeRtf: (rtf: string) => Promise<void>
-  readHtml: () => Promise<string>
-  writeHtml: (html: string) => Promise<void>
-  writeHtmlAndText: (html: string, text: string) => Promise<void>
-  hasText: () => Promise<boolean>
-  hasRTF: () => Promise<boolean>
-  hasHTML: () => Promise<boolean>
-  hasImage: () => Promise<boolean>
-  hasFiles: () => Promise<boolean>
+  readText: typeof clipboard.readText
+  writeText: typeof clipboard.writeText
+  readImageBase64: typeof clipboard.readImageBase64
+  writeImageBase64: typeof clipboard.writeImageBase64
+  readFiles: typeof clipboard.readFiles
+  writeFiles: typeof clipboard.writeFiles
+  readRtf: typeof clipboard.readRtf
+  writeRtf: typeof clipboard.writeRtf
+  readHtml: typeof clipboard.readHtml
+  writeHtml: typeof clipboard.writeHtml
+  writeHtmlAndText: typeof clipboard.writeHtmlAndText
+  hasText: typeof clipboard.hasText
+  hasRTF: typeof clipboard.hasRTF
+  hasHTML: typeof clipboard.hasHTML
+  hasImage: typeof clipboard.hasImage
+  hasFiles: typeof clipboard.hasFiles
 }
 
 export interface INotification {
-  sendNotification: (
-    ...args: Parameters<typeof notification.sendNotification>
-  ) => ReturnType<typeof notification.sendNotification>
-  requestPermission: () => ReturnType<typeof notification.requestPermission>
-  isPermissionGranted: () => ReturnType<typeof notification.isPermissionGranted>
+  sendNotification: typeof notification.sendNotification
+  requestPermission: typeof notification.requestPermission
+  isPermissionGranted: typeof notification.isPermissionGranted
 }
 
 export interface IFs {
-  readDir: (...args: Parameters<typeof fs.readDir>) => ReturnType<typeof fs.readDir>
-  readFile: (...args: Parameters<typeof fs.readFile>) => ReturnType<typeof fs.readFile>
-  readTextFile: (...args: Parameters<typeof fs.readTextFile>) => ReturnType<typeof fs.readTextFile>
-  stat: (...args: Parameters<typeof fs.stat>) => ReturnType<typeof fs.stat>
-  lstat: (...args: Parameters<typeof fs.lstat>) => ReturnType<typeof fs.lstat>
-  exists: (...args: Parameters<typeof fs.exists>) => ReturnType<typeof fs.exists>
-  mkdir: (...args: Parameters<typeof fs.mkdir>) => ReturnType<typeof fs.mkdir>
-  create: (...args: Parameters<typeof fs.create>) => ReturnType<typeof fs.create>
-  copyFile: (...args: Parameters<typeof fs.copyFile>) => ReturnType<typeof fs.copyFile>
-  remove: (...args: Parameters<typeof fs.remove>) => ReturnType<typeof fs.remove>
-  rename: (...args: Parameters<typeof fs.rename>) => ReturnType<typeof fs.rename>
-  truncate: (...args: Parameters<typeof fs.truncate>) => ReturnType<typeof fs.truncate>
-  writeFile: (...args: Parameters<typeof fs.writeFile>) => ReturnType<typeof fs.writeFile>
-  writeTextFile: (
-    ...args: Parameters<typeof fs.writeTextFile>
-  ) => ReturnType<typeof fs.writeTextFile>
+  readDir: typeof fs.readDir
+  readFile: typeof fs.readFile
+  readTextFile: typeof fs.readTextFile
+  stat: typeof fs.stat
+  lstat: typeof fs.lstat
+  exists: typeof fs.exists
+  mkdir: typeof fs.mkdir
+  create: typeof fs.create
+  copyFile: typeof fs.copyFile
+  remove: typeof fs.remove
+  rename: typeof fs.rename
+  truncate: typeof fs.truncate
+  writeFile: typeof fs.writeFile
+  writeTextFile: typeof fs.writeTextFile
 }
 
 export interface IOs {
-  platform: () => ReturnType<typeof os.platform>
-  arch: () => ReturnType<typeof os.arch>
-  exeExtension: () => ReturnType<typeof os.exeExtension>
-  family: () => ReturnType<typeof os.family>
-  hostname: () => ReturnType<typeof os.hostname>
+  platform: typeof os.platform
+  arch: typeof os.arch
+  exeExtension: typeof os.exeExtension
+  family: typeof os.family
+  hostname: typeof os.hostname
   eol: () => Promise<string>
-  version: () => ReturnType<typeof os.version>
-  locale: () => ReturnType<typeof os.locale>
-}
-
-export type ShellxExecutePayload = {
-  program: string
-  args: string[]
-  options: shellx.InternalSpawnOptions
+  version: typeof os.version
+  locale: typeof os.locale
 }
 
 export interface IShell {
@@ -84,19 +75,19 @@ export interface IShell {
   ): Promise<shellx.ChildProcess<shellx.IOPayload>>
   kill(pid: number): Promise<void>
   stdinWrite(buffer: string | number[], pid: number): Promise<void>
-  open(path: string, openWith?: string): Promise<void>
-  makeBashScript(script: string): shellx.Command<string>
-  makePowershellScript(script: string): shellx.Command<string>
-  makeAppleScript(script: string): shellx.Command<string>
-  makePythonScript(script: string): shellx.Command<string>
-  makeZshScript(script: string): shellx.Command<string>
-  makeNodeScript(script: string): shellx.Command<string>
-  executeBashScript(script: string): Promise<shellx.ChildProcess<string>>
-  executePowershellScript(script: string): Promise<shellx.ChildProcess<string>>
-  executeAppleScript(script: string): Promise<shellx.ChildProcess<string>>
-  executePythonScript(script: string): Promise<shellx.ChildProcess<string>>
-  executeZshScript(script: string): Promise<shellx.ChildProcess<string>>
-  executeNodeScript(script: string): Promise<shellx.ChildProcess<string>>
+  open: typeof shellx.open
+  makeBashScript: typeof shellx.makeBashScript
+  makePowershellScript: typeof shellx.makePowershellScript
+  makeAppleScript: typeof shellx.makeAppleScript
+  makePythonScript: typeof shellx.makePythonScript
+  makeZshScript: typeof shellx.makeZshScript
+  makeNodeScript: typeof shellx.makeNodeScript
+  executeBashScript: typeof shellx.executeBashScript
+  executePowershellScript: typeof shellx.executePowershellScript
+  executeAppleScript: typeof shellx.executeAppleScript
+  executePythonScript: typeof shellx.executePythonScript
+  executeZshScript: typeof shellx.executeZshScript
+  executeNodeScript: typeof shellx.executeNodeScript
 }
 
 export type FetchOptions = {
